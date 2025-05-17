c758d95a88b87a29d028056161c18822763e025d
rabouzia <54402804+rabouzia@users.noreply.github.com>
2025-05-17 18:42:37 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 #[allow(unused_imports)]
 use std::net::UdpSocket;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+fn main()
+{
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
-    
+
     loop {
-        match udp_socket.recv_from(&mut buf) {
+        match udp_socket.recv_from(&mut buf)
+        {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let response: [u8; 12]= [0x04, 0xd2, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0];
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
@@ -25,3 +25,7 @@ fn main() {
         }
     }
 }
+
+/*
+dig @127.0.0.1 -p 2053 +noedns codecrafters.io
+*/
